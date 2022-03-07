3b51b79e094851f80268363bdea5f49b54f795c1
code-arman <armanr123@gmail.com>
2022-03-07 04:15:10 -0600

    Reduce scopes

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -1,8 +1,6 @@
 const CLIENT_Id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 const SPOTIFY_AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const SCOPES = [
-  "user-read-private",
-  "user-read-email",
   "playlist-modify-public",
   "playlist-modify-private",
   "playlist-read-private",
