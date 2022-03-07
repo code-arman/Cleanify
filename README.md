3ee005b20ab5d7a92678f79aa47b1e6f1914a05c
code-arman <46976119+code-arman@users.noreply.github.com>
2022-03-07 03:20:25 -0600

    Rewrite Cleanify in React (#3)
    * Add chakra ui
    
    * Rewrite Cleanify in React

--- README.md
+++ README.md
@@ -1,60 +1,70 @@
-# Cleanify
+# Getting Started with Create React App
 
-### What it does:
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-Cleanify converts your Explicit spotify playlists into Clean spotify playlists so you can listen to your favorite playlists when your parents are around!
+## Available Scripts
 
-### How to run it:
+In the project directory, you can run:
 
-1. Login to your [Spotify Developer Dashboard](https://developer.spotify.com/dashboard/applications)
-2. Create a "new app" and record your Client ID and Client Secret
-3. Go to "edit settings" within your project and set the Redirect URI to http://localhost:3000/callback and save
-4. Clone this github repo or download the zip file. This example saves it to your desktop
+### `yarn start`
 
-```
-cd Desktop
-git clone https://github.com/code-arman/Cleanify.git
-cd Cleanify
-```
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
-5. Once you're in the project directory in Terminal, run the following npm command (make sure you already have [Node](https://nodejs.org/en/download/) installed)
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
 
-```
-npm install
-```
+### `yarn test`
 
-6. Add your Client ID and Client Secret that you got from your Spotify developer dashboard into terminal by running the following commands. Replace your information wtih YOURCLIENTID and YOURCLIENTSECRET.
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-![Dashboard](imgs/spotify-developer-dashboard-keys.png)
+### `yarn build`
 
-```
-export CLIENT_ID=YOURCLIENTID
-export CLIENT_SECRET=YOURCLIENTSECRET
-```
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
 
-7. Run the following commands
-   ```
-   cd src
-   node server.js
-   ```
-8. Go to `http://localhost:3000/` in your browser (preferably Google Chrome)
-9. Login with Spotify and Cleanify your Playlists
-10. Your new playlist will show up in your spotify account
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
 
-### How it works:
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-Cleanify sends a request to spotify's api for each song in the selected playlist with " clean" appended at the end. Often times, users have created a playlist with the Clean version of that song inside of it, so it shows at the top of the search results. Cleanify then compares each song in this recenetly searched for playlist to the song that the user has in their own playlist, and adds it to a new 'Cleanified' playlist if it is both non-explicit and has the same name. There is no method to ensure all songs in the playlist have a clean version, but using Cleanify gives you the best chance at finding the clean version if it is there, and removes it if it can't find the clean version of the song. This will make it so when you want to listen to your playlist around your parents, the songs will not have swear words :)
+### `yarn eject`
 
-## How to use it
+**Note: this is a one-way operation. Once you `eject`, you can't go back!**
 
-<h6>Login Screen</h6>
+If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
 
-![Login](imgs/cleanify-login-page.png)
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
 
-<h6>Cleanify Screen</h6>
+You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
 
-![Home](imgs/cleanify-home.png)
+## Learn More
 
-### Credit
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
-Project Idea from [here](https://github.com/Divide-By-0/app-ideas-people-would-use)
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### Analyzing the Bundle Size
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### Making a Progressive Web App
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### Advanced Configuration
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### Deployment
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `yarn build` fails to minify
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,206 @@
+import { Heading, Flex, VStack, Link, Text } from "@chakra-ui/layout";
+import { useContext, useEffect, useState } from "react";
+import {
+  createPlaylist,
+  getTracks,
+  getUser,
+  deletePlaylist,
+  addTracksToPlaylist,
+  searchForTrack,
+  getPlaylists,
+} from "../utils/api.js";
+import {
+  Button,
+  SimpleGrid,
+  Container,
+  useToast,
+  useDisclosure,
+} from "@chakra-ui/react";
+import PlaylistTable from "../components/PlaylistTable.jsx";
+import SongTable from "../components/SongTable.jsx";
+import AuthContext from "../contexts/SpotifyAuthContext.jsx";
+import CleanSongTable from "../components/CleanSongTable.jsx";
+import { SummaryModal } from "../components/SummaryModal.jsx";
+
+const Home = () => {
+  const [user, setUser] = useState();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [cleanifySummary, setCleanifySummary] = useState();
+  const toast = useToast();
+  const {
+    checkedPlaylist,
+    playlists,
+    tracks,
+    setPlaylists,
+    setTracks,
+    cleanedPlaylistID,
+    setCleanedPlaylistID,
+  } = useContext(AuthContext);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const u = await getUser();
+      setUser(u);
+    };
+    loadUser();
+  }, []);
+
+  const handleDelete = async () => {
+    await deletePlaylist(playlists.items[checkedPlaylist].id);
+
+    setPlaylists(await getPlaylists());
+    toast({
+      title: `Deleted Playlist named "${playlists.items[checkedPlaylist].name}"`,
+      position: "top-right",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
+
+  const handleCleanify = async () => {
+    toast({
+      title: `Cleanifying Playlist...`,
+      position: "top-right",
+      status: "info",
+      duration: 20000,
+      isClosable: true,
+    });
+
+    const tracks = await getTracks(playlists.items[checkedPlaylist].id);
+    setTracks(tracks);
+
+    const cleanTrackIDs = [];
+    const explicitTrackNames = [];
+
+    for (let t of tracks.items) {
+      t.track.explicit
+        ? explicitTrackNames.push(`${t.track.name} Clean`)
+        : cleanTrackIDs.push(`spotify:track:${t.track.id}`);
+    }
+
+    const cleanVersionTrackIDs = [];
+    const cleanVersionNames = [];
+
+    for (let track of explicitTrackNames) {
+      const playlistResponses = await searchForTrack(track);
+      if (playlistResponses.playlists.items.length > 0) {
+        const playlistWithCleanSong = playlistResponses.playlists;
+        const firstPlaylistID = playlistWithCleanSong.items[0].id;
+        const cleanTracks = await getTracks(firstPlaylistID);
+        for (let t of cleanTracks.items) {
+          if (
+            t &&
+            t.track &&
+            !t.track.explicit &&
+            t.track.name === track.slice(0, -6)
+          ) {
+            cleanVersionTrackIDs.push(`spotify:track:${t.track.id}`);
+            cleanVersionNames.push(t.track.name);
+          }
+        }
+      }
+    }
+    const newPlaylist = await createPlaylist(
+      `${playlists.items[checkedPlaylist].name} (Cleanified)`,
+      user.id
+    );
+    setPlaylists(await getPlaylists());
+
+    await addTracksToPlaylist(newPlaylist.id, [
+      ...cleanTrackIDs,
+      ...cleanVersionTrackIDs,
+    ]);
+    setCleanifySummary({
+      numOriginalClean: cleanTrackIDs.length,
+      numCleanFound: cleanVersionTrackIDs.length,
+      cleanFound: cleanVersionNames,
+    });
+    toast.closeAll();
+
+    setCleanedPlaylistID(newPlaylist.id);
+
+    toast({
+      title: "Sucessfully Cleanified Playlist",
+      position: "top-right",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
+
+  return (
+    <Flex align="center" justify="center" p={[0, 1, 15, 20]}>
+      <VStack>
+        {user && <Heading>Cleanify</Heading>}
+        {user && <Text fontSize="lg">{`Username: ${user.display_name}`}</Text>}
+        {!user && (
+          <>
+            <Heading fontSize="xl">{`You need to login into spotify! `}</Heading>
+            <Link href="/">Click here to go to the login page</Link>
+          </>
+        )}
+
+        {user && (
+          <SimpleGrid spacing={[1, 3, 5, 5]} columns={[1, 1, 3, 3]}>
+            <Button colorScheme="green" onClick={handleCleanify}>
+              Cleanify Playlist
+            </Button>
+            <Button colorScheme="red" onClick={handleDelete}>
+              Delete Playlist
+            </Button>
+            <Button isDisabled={!cleanedPlaylistID} onClick={onOpen}>
+              View Summary
+            </Button>
+          </SimpleGrid>
+        )}
+        {cleanifySummary && (
+          <SummaryModal
+            isOpen={isOpen}
+            onClose={onClose}
+            details={cleanifySummary}
+          />
+        )}
+        <SimpleGrid columns={[1, 1, 1, 3]} alignItems="center" spacing={5}>
+          <Container
+            mt={[20, 1, 1, 1]}
+            mb={[20, 1, 1, 1]}
+            h="700px"
+            width={["200px", "300px", "400px"]}
+          >
+            {user && <PlaylistTable />}
+          </Container>
+          <Container
+            mt={["150px", 1, 1, 1]}
+            mb={[20, 1, 1, 1]}
+            h="700px"
+            width={["200px", "300px", "400px"]}
+          >
+            {tracks && (
+              <SongTable
+                title={`Before Cleanified (${tracks.items.length} songs)`}
+              />
+            )}
+          </Container>
+          <Container
+            mt={[0, 1, 1, 1]}
+            mb={[20, 1, 1, 1]}
+            h="700px"
+            width={["200px", "300px", "400px"]}
+          >
+            {cleanedPlaylistID && (
+              <CleanSongTable
+                title={`After Cleanified (${
+                  cleanifySummary.numCleanFound +
+                  cleanifySummary.numOriginalClean
+                } songs)`}
+              />
+            )}
+          </Container>
+        </SimpleGrid>
+      </VStack>
+    </Flex>
+  );
+};
+
+export default Home;
